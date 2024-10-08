df2241cd510c6495c388aa89da04d31e6663e56c
avysotsk <vusota98@gmail.com>
2024-10-08 20:13:15 +0300

    rewritted to TS index and fetchHtml. added config ts file

--- front/src/modules/Navigation/index.tsx
+++ front/src/modules/Navigation/index.tsx
@@ -4,27 +4,6 @@ import { AppBar, Toolbar, Typography, List, ListItem } from "@mui/material";
 const Navigation: React.FC = () => {
 	return (
 		<AppBar position="static">
-			<TabContext value={value}>
-				<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-					<TabList onChange={handleChange} aria-label="lab API tabs example">
-						<Tab label="Item One" value="1" />
-						<Tab label="Item Two" value="2" />
-						<Tab label="Item Three" value="3" />
-					</TabList>
-				</Box>
-				<TabPanel value="1">Item One</TabPanel>
-				<TabPanel value="2">Item Two</TabPanel>
-				<TabPanel value="3">Item Three</TabPanel>
-			</TabContext>
-			<CustomTabPanel value={value} index={0}>
-				Item One
-			</CustomTabPanel>
-			<CustomTabPanel value={value} index={1}>
-				Item Two
-			</CustomTabPanel>
-			<CustomTabPanel value={value} index={2}>
-				Item Three
-			</CustomTabPanel>
 			<Toolbar>
 				<Typography variant="h6" component="div">
 					Navigation
